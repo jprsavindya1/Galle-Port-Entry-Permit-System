967c721dbfe7fd597bfde1f849e5a4885f46019a
sahanSS98 <it20149208@my.sliit.lk>
2025-11-05 00:37:34 +0530

    Add migration to change payment_date from date to datetime

---
+++ database/migrations/2025_11_05_003705_change_payment_date_to_datetime_in_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            // Change payment_date from date to datetime
+            $table->dateTime('payment_date')->default(now())->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            // Revert back to date
+            $table->date('payment_date')->default(now())->change();
+        });
+    }
+};
